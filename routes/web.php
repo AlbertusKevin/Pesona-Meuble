ffd1b39e5ce621acd51e48d0fc74374b1286dd1a
mikhaelAPG <iyel2000@gmail.com>
2020-11-05 11:35:16 +0700

    first commit

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/home', function () {
+    return view('home');
+});
+Route::get('/customer', function () {
+    return view('customer');
+});
+Route::get('/listSalesOrder', function () {
+    return view('listSalesOrder');
+});
+Route::get('/createSalesOrder', function () {
+    return view('createSalesOrder');
+});
