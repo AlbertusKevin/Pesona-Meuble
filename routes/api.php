ff5cd34b48880143b3ebbd0701dc44142e52c068
vin-albertus <vinalbertus@gmail.com>
2020-12-26 00:20:24 +0700

    menambahkan RESTful API untuk employee

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Domain\Procurement\Entity\Meuble;
+use App\Domain\Employee\Entity\Employee;
+use Illuminate\Support\Facades\Hash;
 
 /*
 |--------------------------------------------------------------------------
@@ -24,13 +26,119 @@ Route::get('meubles', function () {
     if ($meubles) {
         return response()->json([
             'status' => true,
-            'meubles' => Meuble::all(),
+            'meubles' => $meubles,
             'message' => 'Data berhasil didapat.'
         ], 200);
     } else {
         return response()->json([
             'status' => false,
             'message' => 'Data meuble tidak ditemukan.'
+        ], 404);
+    }
+});
+
+Route::get('meubles/{model}', function ($model) {
+    $meuble = Meuble::where('modelType', $model)->first();
+    if ($meuble) {
+        return response()->json([
+            'status' => true,
+            'meuble' => $meuble,
+            'message' => 'Data meuble dengan model ' . $meuble->modelType . ' berhasil didapat.'
+        ], 200);
+    } else {
+        return response()->json([
+            'status' => false,
+            'message' => 'Data meuble tidak ditemukan.'
+        ], 404);
+    }
+});
+
+
+//api untuk data employee
+Route::get('employee', function () {
+    $employees = Employee::select('name', 'email', 'phone', 'address', 'status')->get();
+    if ($employees) {
+        return response()->json([
+            'status' => 'success',
+            'employees' => $employees,
+            'message' => 'Berhasil meminta data seluruh pekerja'
+        ], 200);
+    } else {
+        return response()->json([
+            'status' => false,
+            'message' => 'Data pekerja tidak ditemukan.'
+        ], 404);
+    }
+});
+
+Route::get('employee/{id}', function ($id) {
+    $employee = Employee::select('name', 'email', 'phone', 'address', 'status')->where('id', $id)->first();
+    if ($employee) {
+        return response()->json([
+            'status' => 'success',
+            'employee' => $employee,
+            'message' => 'Berhasil meminta data pekerja ' . $employee->name
+        ], 200);
+    } else {
+        return response()->json([
+            'status' => false,
+            'message' => 'Data pekerja tidak ditemukan.'
+        ], 404);
+    }
+});
+
+Route::post('employee', function (Request $request) {
+    try {
+        $employee = Employee::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'phone' => $request->phone,
+            'address' => $request->address,
+            'password' => Hash::make($request->password),
+            'role' => $request->role,
+            'raiseIteration' => 0,
+            'status' => 1,
+            'salary' => 3500000
+        ]);
+
+        return response()->json([
+            'status' => 'success',
+            'employee' => $employee,
+            'message' => 'Berhasil menambah data pekerja ' . $request->name
+        ], 201);
+    } catch (Throwable $e) {
+        return response()->json([
+            'status' => false,
+            'message' => 'Gagal menambahkan data karyawan.',
+            'error' => $e
+        ], 400);
+    }
+});
+
+Route::put('employee/{id}', function ($id, Request $request) {
+    $employee = Employee::where('id', $id)->update($request->all());
+    if ($employee) {
+        return response()->json([
+            'status' => 'success',
+            'employee' => $employee,
+            'message' => 'Berhasil mengupdate data pekerja ' . $request->name
+        ], 204);
+    } else {
+        return response()->json([
+            'status' => false,
+            'message' => 'Data pekerja tidak ditemukan.'
+        ], 404);
+    }
+});
+
+Route::delete('employee/{id}', function ($id) {
+    $employee = Employee::where('id', $id)->delete();
+    if ($employee) {
+        return response()->json(null, 204);
+    } else {
+        return response()->json([
+            'status' => false,
+            'message' => 'Data pekerja tidak ditemukan.'
         ], 404);
     }
 });
