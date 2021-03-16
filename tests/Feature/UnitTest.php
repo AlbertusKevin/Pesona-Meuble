6ffec7ed1ae6c24c5a72eae1735c90df5b00383f
vin-albertus <vinalbertus@gmail.com>
2021-03-16 15:29:37 +0700

    Coba testing 2

---
+++ tests/Feature/UnitTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Domain\CustomerManagement\Service\CustomerService;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Http\Controllers\Customer\CustomerController;
+use Illuminate\Support\Facades\Request;
+use Illuminate\Support\Facades\Validator;
+
+class UnitTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    // public function testExample()
+    // {
+    //     $response = $this->get('/');
+
+    //     $response->assertStatus(200);
+    // }
+
+    // public function testPostCustomer()
+    // {
+    //     $response = $this->from('customer/create')->json('POST', 'customer', [
+    //         'name' => "Martin",
+    //         'email' => "martin",
+    //         'phone' => "0819283212",
+    //         'address' => "Jl. Soetta 23"
+    //     ]);
+
+    //     $response->assertStatus(302);
+    //     $response->assertRedirect('customer');
+    // }
+
+    public function testQueryListCustomer()
+    {
+        $customer = new CustomerService();
+        $this->assertCount(2, $customer->index_customers());
+    }
+
+    public function testQueryShowCustomer()
+    {
+        $customer = new CustomerService();
+        $this->assertNull($customer->customer_by_id(3));
+    }
+}
