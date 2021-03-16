716f512dfe18d9c5f22ff9db349f913d6d98a413
Chris Christian <urgood71@yahoo.com>
2021-03-16 23:14:02 +0700

    Login Test Added

--- tests/Feature/UnitTest.php
+++ tests/Feature/UnitTest.php
@@ -24,6 +24,7 @@ class UnitTest extends TestCase
     //     $response->assertStatus(200);
     // }
 
+
     // public function testPostCustomer()
     // {
     //     $response = $this->from('customer/create')->json('POST', 'customer', [
@@ -40,7 +41,9 @@ class UnitTest extends TestCase
     public function testQueryListCustomer()
     {
         $customer = new CustomerService();
-        $this->assertCount(2, $customer->index_customers());
+        $this->assertCount(3, $customer->index_customers());
+
+        // Test Failed, It should be 3.
     }
 
     public function testQueryShowCustomer()
@@ -48,4 +51,44 @@ class UnitTest extends TestCase
         $customer = new CustomerService();
         $this->assertNull($customer->customer_by_id(3));
     }
+
+    //TEST LOGIN 
+    public function testLoginPage()
+    {
+        $response = $this->get('/gate');
+
+        $this->assertEquals(200, $response->status());
+    }
+
+    public function testLoginService() { 
+        $response = $this->post('/gate', [
+            'email' => 'albertus@gmail.com',
+            'password' => 'test',
+        ]);
+        $response->assertSessionMissing('errors');
+
+        //Passwordnya lupa
+    }
+
+    public function testLogout() { 
+        $response = $this->withSession(['login' => true, 'id_employee' => 1])->get('/logout');
+        $response->assertRedirect('/');
+    }
+
+    //Cek validasi jika input email dan password yang tidak sesuai dengan Validator
+    public function testLoginValidation() { 
+        $response = $this->post('/gate', [
+            'email' => 'test',
+            'password' => '',
+        ]);
+        $response->assertSessionHasErrors(['email', 'password']);
+    }
+
+    // masih sedikit error
+    public function testCancelSalesOrder() { 
+        $this->withSession(['login' => true, 'id_employee' => 1])
+            ->patch('/salesorder/cancel/2000001')
+            ->assertRedirect('/salesorder')->with(['success' => 'Sales Order  2000001 canceled !']);;
+    }
+
 }
